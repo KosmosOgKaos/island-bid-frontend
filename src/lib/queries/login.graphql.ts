e2575afe3db9119f1fce31c117ac9ba2b7cb0f39
krillijuni <kristleifur@juni.is>
2025-05-06 15:47:09 +0000

    Login button now calls backend login function

---
+++ src/lib/queries/login.graphql.ts
@@ -0,0 +1,9 @@
+import { graphql } from '@/graphql/gql'
+
+export const login = graphql(`
+  mutation Login($phoneNumber: String!) {
+    login(input: {phoneNumber: $phoneNumber}) {
+      success
+    }
+  }
+`)
