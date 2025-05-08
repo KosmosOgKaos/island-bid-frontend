9957f27ec64dbfac48af54441fe3860ab9efe53b
albina <albina@kosmosogkaos.is>
2025-05-08 08:02:29 +0000

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Ísland.is Útboðsverkefni 2025',
   description: 'Island.is UI Components',
+  icons: {
+    icon: '/favicon.svg',
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
